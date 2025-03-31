bb5efbcd00d62fb214705f64942ca493d3693187
Nicolas Bredeche <nekonaute@users.noreply.github.com>
2025-03-31 12:22:08 +0200

    Update robot_wanderer.py

--- robot_wanderer.py
+++ robot_wanderer.py
@@ -2,7 +2,7 @@
 from robot import * 
 
 nb_robots = 0
-debug = False
+debug = True
 
 class Robot_player(Robot):
 
@@ -17,8 +17,10 @@ class Robot_player(Robot):
         super().__init__(x_0, y_0, theta_0, name=name, team=team)
 
     def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
-        translation = sensors[sensor_front]*0.8
-        rotation = 1.0 * sensors[sensor_front_left] - 1.0 * sensors[sensor_front_right]
+        
+        translation = sensors[sensor_front]*0.8 #  A MODIFIER
+        rotation = 1.0 * sensors[sensor_front_left] - 1.0 * sensors[sensor_front_right] #  A MODIFIER
+
         if debug == True:
             if self.iteration % 100 == 0:
                 print ("Robot",self.robot_id," (team "+str(self.team_name)+")","at step",self.iteration,":")
@@ -26,5 +28,6 @@ class Robot_player(Robot):
                 print ("\ttype (0:empty, 1:wall, 2:robot) =",sensor_view)
                 print ("\trobot's name (if relevant)      =",sensor_robot)
                 print ("\trobot's team (if relevant)      =",sensor_team)
+
         self.iteration = self.iteration + 1        
         return translation, rotation, False
